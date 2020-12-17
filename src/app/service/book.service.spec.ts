c11cbf57a880442212d968abafadbc9d3251ec88
Michal Czyzowicz <czyzowicz.m@gmail.com>
2020-12-17 17:42:44 +0100

    add tests for book service

--- src/app/service/book.service.spec.ts
+++ src/app/service/book.service.spec.ts
@@ -23,4 +23,24 @@ describe('BookService', () => {
 
     expect(url).toBe('intitle:Lord of the rings');
   });
+
+  it('checks if search starts when criteria are fulfilled', () => {
+    service.authorSubject$.next('J.K. Rowling');
+    const url = service.buildUrl();
+
+    expect(url).toBe('inauthor:J.K. Rowling');
+  });
+
+  it('checks if search starts when criteria are fulfilled', () => {
+    service.publisherSubject$.next('New Era');
+    const url = service.buildUrl();
+
+    expect(url).toBe('inpublisher:New Era');
+  });
+
+  it('checks if url before builing is empty', () => {
+    const url = service.buildUrl();
+
+    expect(url).toBe('');
+  });
 });
